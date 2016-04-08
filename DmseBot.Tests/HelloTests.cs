5bec19190e46ba41bf02532cb80d4d1567004ee9
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-04-08 01:46:10 -0400

    Add Simple Test Project

---
+++ DmseBot.Tests/HelloTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DmseBot.Tests
+{
+    public class HelloTests
+    {
+        [Fact]
+        public void Test_Tests_Run()
+        {
+            Assert.True(true);
+        }
+    }
+}
